18ccc30455ed73cf8e63228b93947ab938a7cbc2
Matthew Horridge <matthew.horridge@stanford.edu>
2021-04-08 13:41:37 +0100

    Fixed serialization

--- webprotege-gwt-ui-shared/src/main/java/edu/stanford/bmir/protege/web/shared/merge/MergeUploadedProjectResult.java
+++ webprotege-gwt-ui-shared/src/main/java/edu/stanford/bmir/protege/web/shared/merge/MergeUploadedProjectResult.java
@@ -1,5 +1,9 @@
 package edu.stanford.bmir.protege.web.shared.merge;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.google.auto.value.AutoValue;
+import com.google.common.annotations.GwtCompatible;
 import edu.stanford.bmir.protege.web.shared.dispatch.Result;
 
 /**
@@ -7,12 +11,13 @@ import edu.stanford.bmir.protege.web.shared.dispatch.Result;
  * Stanford Center for Biomedical Informatics Research
  * 26/01/15
  */
-public class MergeUploadedProjectResult implements Result {
+@AutoValue
+@GwtCompatible(serializable = true)
+@JsonTypeName("MergeUploadedProject")
+public abstract class MergeUploadedProjectResult implements Result {
 
-    private MergeUploadedProjectResult() {
-    }
-
+    @JsonCreator
     public static MergeUploadedProjectResult create() {
-        return new MergeUploadedProjectResult();
+        return new AutoValue_MergeUploadedProjectResult();
     }
 }
