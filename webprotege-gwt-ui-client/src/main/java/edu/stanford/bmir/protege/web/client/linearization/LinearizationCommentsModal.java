a32f4ae9c9adfdc32aadcc6680cc9820eea7559b
alexsilaghi <silaghialexmihail@gmail.com>
2024-08-12 14:50:12 +0300

    changed title

--- webprotege-gwt-ui-client/src/main/java/edu/stanford/bmir/protege/web/client/linearization/LinearizationCommentsModal.java
+++ webprotege-gwt-ui-client/src/main/java/edu/stanford/bmir/protege/web/client/linearization/LinearizationCommentsModal.java
@@ -31,7 +31,7 @@ public class LinearizationCommentsModal {
         presenter.setView(view);
         presenter.setEscapeButton(DialogButton.CANCEL);
         presenter.setPrimaryButton(DialogButton.OK);
-        presenter.setTitle("Linearization comments");
+        presenter.setTitle("Coding notes");
         presenter.setButtonHandler(DialogButton.OK, closer -> {
             closer.closeModal();
             String body = view.getBody();
