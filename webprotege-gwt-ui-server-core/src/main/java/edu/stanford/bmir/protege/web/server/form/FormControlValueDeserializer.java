21a303858ac44b11fd30658908b77cf2c69d55c4
Matthew Horridge <matthew.horridge@stanford.edu>
2024-09-05 10:55:26 -0700

    Fixes #57
    Allow non-prefixed names for types

--- webprotege-gwt-ui-server-core/src/main/java/edu/stanford/bmir/protege/web/server/form/FormControlValueDeserializer.java
+++ webprotege-gwt-ui-server-core/src/main/java/edu/stanford/bmir/protege/web/server/form/FormControlValueDeserializer.java
@@ -53,16 +53,23 @@ public class FormControlValueDeserializer extends StdDeserializer<PrimitiveFormC
                     }
                     switch (type) {
                         case "owl:Class" :
+                        case "Class" :
                             return PrimitiveFormControlData.get(df.getOWLClass(iri));
                         case "owl:ObjectProperty":
+                        case "ObjectProperty" :
                             return PrimitiveFormControlData.get(df.getOWLObjectProperty(iri));
                         case "owl:DatatypeProperty" :
+                        case "DatatypeProperty" :
+                        case "DataProperty" :
                             return PrimitiveFormControlData.get(df.getOWLDataProperty(iri));
                         case "owl:AnnotationProperty":
+                        case "AnnotationProperty" :
                             return PrimitiveFormControlData.get(df.getOWLAnnotationProperty(iri));
                         case "rdfs:Datatype" :
+                        case "Datatype" :
                             return PrimitiveFormControlData.get(df.getOWLDatatype(iri));
                         case "owl:NamedIndividual" :
+                        case "NamedIndividual"     :
                             return PrimitiveFormControlData.get(df.getOWLNamedIndividual(iri));
                     }
                     throw new JsonParseException(p, "Unrecognised entity type: " + type);
