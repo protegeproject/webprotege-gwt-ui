80053dff1484e62546d698ffe1be3d58f2bb3229
alexsilaghi <silaghialexmihail@gmail.com>
2024-10-01 11:43:31 +0300

    added tooltip.

--- webprotege-gwt-ui-client/src/main/java/edu/stanford/bmir/protege/web/client/postcoordination/PostCoordinationCheckboxConfig.java
+++ webprotege-gwt-ui-client/src/main/java/edu/stanford/bmir/protege/web/client/postcoordination/PostCoordinationCheckboxConfig.java
@@ -9,8 +9,6 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class PostCoordinationCheckboxConfig extends CheckBoxConfig {
-    private final Logger logger = Logger.getLogger("CheckBoxConfig");
-
     private final static String CHECK_SVG = "<svg viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\" stroke=\"#2cd900\"><g id=\"SVGRepo_bgCarrier\" stroke-width=\"0\"></g><g id=\"SVGRepo_tracerCarrier\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></g><g id=\"SVGRepo_iconCarrier\"> <path d=\"M4 12.6111L8.92308 17.5L20 6.5\" stroke=\"#2cd900\" stroke-width=\"2.4\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path> </g></svg>";
     private final static String REQUIRED_SVG = "<svg viewBox=\"0 0 512 512\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" fill=\"#000000\"><g id=\"SVGRepo_bgCarrier\" stroke-width=\"0\"></g><g id=\"SVGRepo_tracerCarrier\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></g><g id=\"SVGRepo_iconCarrier\"> <title>mandatory-done</title> <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\"> <g id=\"icon\" fill=\"#c23f42\" transform=\"translate(114.773333, 42.666667)\"> <path d=\"M322.588167,146.773333 L367.843001,192.028167 L130.588167,429.283001 L-2.61257984e-15,298.694834 L45.254834,253.44 L130.588167,338.780167 L322.588167,146.773333 Z M169.347879,7.10542736e-15 L159.927965,83.3939394 L236.94961,49.3160173 L253.850043,103.619048 L172.672554,119.411255 L226.975584,180.08658 L180.707186,213.333333 L141.088139,141.298701 L101.746147,213.333333 L55.2006926,180.08658 L109.503723,119.411255 L28.60329,103.619048 L45.2266667,49.3160173 L122.248312,83.3939394 L112.828398,7.10542736e-15 L169.347879,7.10542736e-15 Z\" id=\"Combined-Shape\"> </path> </g> </g> </g></svg>";
 
@@ -24,19 +22,19 @@ public class PostCoordinationCheckboxConfig extends CheckBoxConfig {
     private CheckboxValue parentValue;
     private boolean isDerived = false;
 
-    public static List<CheckboxValue> AVAILABLE_VALUES_LIST = Arrays.asList(new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED"),
-            new CheckboxValue(CHECK_SVG, "ALLOWED"),
-            new CheckboxValue(REQUIRED_SVG, "REQUIRED")
+    public static List<CheckboxValue> AVAILABLE_VALUES_LIST = Arrays.asList(new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED", "This axis cannot be used as a postcoordination axis for this linearization."),
+            new CheckboxValue(CHECK_SVG, "ALLOWED", "This axis can be used as a postcoordination axis for this linearization."),
+            new CheckboxValue(REQUIRED_SVG, "REQUIRED", "This axis is required to be used as a postcoordination axis for this linearization.")
             );
 
     public static List<CheckboxValue> AVAILABLE_DEFAULT_VALUE_LIST = Arrays.asList(
-            new CheckboxValue(DEFAULT_NOT_ALLOWED, "DEFAULT_NOT_ALLOWED"),
-            new CheckboxValue(DEFAULT_CHECK_SVG, "DEFAULT_ALLOWED"),
-            new CheckboxValue(DEFAULT_REQUIRED_SVG, "DEFAULT_REQUIRED")
+            new CheckboxValue(DEFAULT_NOT_ALLOWED, "DEFAULT_NOT_ALLOWED", "This axis inherits its behavior from its root linearization: This axis cannot be used as a postcoordination axis for this linearization."),
+            new CheckboxValue(DEFAULT_CHECK_SVG, "DEFAULT_ALLOWED", "This axis inherits its behavior from its root linearization: This axis can be used as a postcoordination axis for this linearization."),
+            new CheckboxValue(DEFAULT_REQUIRED_SVG, "DEFAULT_REQUIRED", "This axis inherits its behavior from its root linearization: This axis is required to be used as a postcoordination axis for this linearization.")
     );
 
     protected PostCoordinationCheckboxConfig() {
-        super(new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED"), AVAILABLE_VALUES_LIST);
+        super(new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED", "This axis cannot be used as a postcoordination axis for this linearization."), AVAILABLE_VALUES_LIST);
     }
 
     @Override
@@ -52,7 +50,7 @@ public class PostCoordinationCheckboxConfig extends CheckBoxConfig {
     @Override
     public CheckboxValue findValue(String inputValue) {
         if(inputValue == null || inputValue.isEmpty()) {
-            return new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED");
+            return new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED", "This axis cannot be used as a postcoordination axis for this linearization.");
         }
         List<CheckboxValue> allValues = new ArrayList<>();
         allValues.addAll(AVAILABLE_VALUES_LIST);
@@ -60,7 +58,7 @@ public class PostCoordinationCheckboxConfig extends CheckBoxConfig {
 
         return allValues.stream().filter(value -> value.getValue().equalsIgnoreCase(inputValue))
                 .findFirst()
-                .orElse(new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED"));
+                .orElse(new CheckboxValue(NOT_ALLOWED, "NOT_ALLOWED", "This axis cannot be used as a postcoordination axis for this linearization."));
     }
 
     private CheckboxValue getNextValueForDerivedClasses(CheckboxValue checkboxValue) {
