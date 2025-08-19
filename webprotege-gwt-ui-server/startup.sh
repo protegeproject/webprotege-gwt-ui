1821e7eebd23d95e55f7b32107ef098a5599ebca
alexsilaghi <silaghialexmihail@gmail.com>
2025-08-19 16:46:07 +0300

    changed startup

--- webprotege-gwt-ui-server/startup.sh
+++ webprotege-gwt-ui-server/startup.sh
@@ -3,12 +3,19 @@
 # Set default context path if not provided
 CONTEXT_PATH=${CONTEXT_PATH:-/webprotege}
 
-# Create the context.xml file dynamically
-cat > $CATALINA_HOME/conf/Catalina/localhost/context.xml << EOF
-<Context path="${CONTEXT_PATH}">
-    <Valve className="org.keycloak.adapters.tomcat.KeycloakAuthenticatorValve"/>
-</Context>
-EOF
+# Remove leading slash for directory name
+CONTEXT_DIR=$(echo $CONTEXT_PATH | sed 's|^/||')
+
+# Copy the WAR file to the correct location based on context path
+if [ "$CONTEXT_PATH" != "/webprotege" ]; then
+    # For custom context path, copy WAR to a directory named after the context
+    mkdir -p $CATALINA_HOME/webapps/$CONTEXT_DIR
+    cp $CATALINA_HOME/webapps/webprotege.war $CATALINA_HOME/webapps/$CONTEXT_DIR/
+    # Extract the WAR file
+    cd $CATALINA_HOME/webapps/$CONTEXT_DIR
+    jar -xf webprotege.war
+    rm webprotege.war
+fi
 
 # Start Tomcat
 exec catalina.sh run
